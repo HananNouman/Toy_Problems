dc3993d3bc01c68c004642531c5f2681f3296fa3
HananNouman <hnouman@hotmail.co.uk>
2018-02-25 09:37:44 +0200

    strings

--- strings.js
+++ strings.js
@@ -2,13 +2,27 @@
 // 	Input :: "a"
 // 	Ouput :: "Position of alphabet: 1"
 	function characPosit(character){
-		//your code is here
-	}
+		var alphabetarray=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+    "w", "x", "y", "z"]
+       var char=character.toLowerCase();
+             if (alphabetarray.indexOf(char)+1 < 1){
+         	     return " its not one of the alphabet letters "
+                }
+		 return  "Position of alphabet: "+(alphabetarray.indexOf(char)+1)
+		}
+	
 
 // Write a function called repeatStr which repeats the given
 // string string exactly n times.
 // repeatStr("hello",4) ==> 'hellohellohellohello'
 	
 	function repeatStr (n, s) {
-		//your code is here
+		var result="";
+		for (var i=0;i<s;i++){
+			result=result+n;
+
+		}
+		return result;
+		
 	}
